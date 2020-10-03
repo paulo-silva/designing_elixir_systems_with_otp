d1d6684b08a832379a8215377dd37deb54c8d1e6
Paulo da Silva <paulo_silvalearn@hotmail.com>
2020-10-03 10:08:44 -0300

    Chapter 4.

--- mastery/lib/mastery/core/question.ex
+++ mastery/lib/mastery/core/question.ex
@@ -3,8 +3,42 @@ defmodule Mastery.Core.Question do
   Represent a question
   """
 
+  alias Mastery.Core.Template
+
   # asked (string): The question text, for example, "1 + 2"
   # template (Template.t): The template that created the question
   # substitutions (%{substitution: any}): Values that will be placed on template.
   defstruct ~w[asked template substitutions]a
+
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
+  defp build_substitution({name, choices_or_generator}) do
+    {name, choose(choices_or_generator)}
+  end
+
+  defp choose(choices) when is_list(choices) do
+    Enum.random(choices)
+  end
+
+  defp choose(generator) when is_function(generator) do
+    generator.()
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      substitutions: substitutions,
+      template: template
+    }
+  end
+
+  defp compile(template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
 end
--- mastery/lib/mastery/core/template.ex
+++ mastery/lib/mastery/core/template.ex
@@ -11,4 +11,13 @@ defmodule Mastery.Core.Template do
   # - generators (%{substitution: list or function}): The generator for each substitution in a template.
   # - checker (function(substitutions, string) -> boolean): Validate if the answer is correct
   defstruct ~w[name category instructions raw compiled generators checker]a
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+
+    struct!(
+      __MODULE__,
+      Keyword.put(fields, :compiled, EEx.compile_string(raw))
+    )
+  end
 end
