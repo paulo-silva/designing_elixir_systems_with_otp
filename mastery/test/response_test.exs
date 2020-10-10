20cf841fdcfb42b4c2a01af240cc8e3232577831
Paulo da Silva <paulo_silvalearn@hotmail.com>
2020-10-10 09:55:49 -0300

    Add initial test for Response.

---
+++ mastery/test/response_test.exs
@@ -0,0 +1,36 @@
+defmodule ResponseTest do
+  use ExUnit.Case
+  use QuizBuilders
+
+  setup [:right, :wrong]
+
+  test "building responses checks answers", %{right: right, wrong: wrong} do
+    assert right.correct
+    refute wrong.correct
+  end
+
+  test "a timestamp is added at build time", %{right: response} do
+    assert %DateTime{} = response.timestamp
+    assert response.timestamp < DateTime.utc_now()
+  end
+
+  defp quiz() do
+    fields = template_fields(generators: %{left: [1], right: [2]})
+
+    build_quiz()
+    |> Quiz.add_template(fields)
+    |> Quiz.select_question()
+  end
+
+  defp response(answer) do
+    Response.new(quiz(), "mathy@example.com", answer)
+  end
+
+  def right(context) do
+    {:ok, Map.put(context, :right, response("3"))}
+  end
+
+  def wrong(context) do
+    {:ok, Map.put(context, :wrong, response("2"))}
+  end
+end
